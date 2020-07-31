7ab3d224f3804d0ac0af06139a0f1b99b7e3785b
alex <alexsandr168@gmail.com>
2020-07-31 12:36:05 +0300

    add project

---
+++ src/test/java/ru/netology/statisticServiceTest/StatisticSevriceTest.java
@@ -0,0 +1,21 @@
+package ru.netology.statisticServiceTest;
+
+import org.junit.jupiter.api.Test;
+import ru.netology.statisticService.StatisticSevrice;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatisticSevriceTest {
+
+    @Test
+    void findMax() {
+        StatisticSevrice sevrice = new StatisticSevrice();
+
+        long [] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = sevrice.findMax(incomesInBillions);
+
+                assertEquals (expected, actual);
+    }
+}
